da27723c06726f101f2ad874d1aaa01cfc65a468
Bharat <bharateffect@gmail.com>
2016-09-18 23:24:17 -0700

    More work on Server Sent Events connection

--- app/src/main/java/com/bsunk/myhome/MainActivity.java
+++ app/src/main/java/com/bsunk/myhome/MainActivity.java
@@ -1,8 +1,11 @@
 package com.bsunk.myhome;
 
 import android.content.Intent;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import com.bsunk.myhome.service.ConfigDataPullService;
 import com.bsunk.myhome.service.EventSourceConnection;
@@ -13,7 +16,7 @@ import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EventSourceConnection sseHandler = new EventSourceConnection();
+    private EventSourceConnection sseHandler = new EventSourceConnection(this);
     private EventSource eventSource;
     String eventUrl = "http://192.168.10.101:8123/api/stream";
     Map<String, String> extraHeaderParameters = new HashMap<>();
@@ -22,6 +25,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+        DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle mDrawerToggle = new ActionBarDrawerToggle(
+                this,  drawerLayout, mToolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close
+        );
+        drawerLayout.setDrawerListener(mDrawerToggle);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        mDrawerToggle.syncState();
 
         Intent service = new Intent(this, ConfigDataPullService.class);
         startService(service);
--- app/src/main/java/com/bsunk/myhome/service/EventSourceConnection.java
+++ app/src/main/java/com/bsunk/myhome/service/EventSourceConnection.java
@@ -1,17 +1,27 @@
 package com.bsunk.myhome.service;
 
-import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.Context;
 import android.util.Log;
+
+import com.bsunk.myhome.data.MyHomeContract;
 import com.tylerjroach.eventsource.EventSourceHandler;
 import com.tylerjroach.eventsource.MessageEvent;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
  * Created by Bharat on 9/18/2016.
  */
 
 public class EventSourceConnection implements EventSourceHandler {
+    Context mContext;
+    public static String LOG_TAG = EventSourceConnection.class.getName();
 
-    public EventSourceConnection() {
+    public EventSourceConnection(Context context) {
+        mContext = context;
     }
 
     @Override
@@ -23,6 +33,60 @@ public class EventSourceConnection implements EventSourceHandler {
     public void onMessage(String event, MessageEvent message) {
         Log.v("SSE Message", message.data);
 
+        try {
+            JSONObject jsonObject = new JSONObject(message.data);
+            if (jsonObject.getString("event_type").equals("state_changed")) {
+                JSONObject data = jsonObject.getJSONObject("data");
+                JSONObject new_state = data.getJSONObject("new_state");
+                if(new_state.getString("entity_id").contains("light.")) {
+                    getLightsUpdate(new_state);
+                }
+                else if(new_state.getString("entity_id").contains("sensor.")) {
+                    getSensorsUpdate(new_state);
+                }
+            }
+        }
+        catch (JSONException e) {
+
+        }
+    }
+
+    public void getSensorsUpdate(JSONObject data) {
+        try {
+            ContentValues values = new ContentValues();
+            String entity_id = data.getString("entity_id");
+            values.put(MyHomeContract.MyHome.COLUMN_LAST_CHANGED, data.getString("last_changed"));
+            values.put(MyHomeContract.MyHome.COLUMN_STATE, data.getString("state"));
+            mContext.getContentResolver().update(MyHomeContract.MyHome.CONTENT_URI, values, MyHomeContract.MyHome.COLUMN_ENTITY_ID + "="+ "'" + entity_id + "'", null);
+        }
+        catch (JSONException e) {
+            Log.e(LOG_TAG, "Failed to parse new sensor state data");
+        }
+    }
+
+    public void getLightsUpdate(JSONObject data) {
+        try {
+            ContentValues values = new ContentValues();
+            String entity_id = data.getString("entity_id");
+            values.put(MyHomeContract.MyHome.COLUMN_LAST_CHANGED, data.getString("last_changed"));
+            values.put(MyHomeContract.MyHome.COLUMN_STATE, data.getString("state"));
+            JSONObject attributes = data.getJSONObject("attributes");
+
+            if (attributes.has("brightness")) {
+                values.put(MyHomeContract.MyHome.COLUMN_BRIGHTNESS, attributes.getString("brightness"));
+            }
+            if (attributes.has("color_temp")) {
+                values.put(MyHomeContract.MyHome.COLUMN_COLOR_TEMP, attributes.getString("color_temp"));
+            }
+            if (attributes.has("rgb_color")) {
+                String rgb = attributes.getString("rgb_color");
+                values.put(MyHomeContract.MyHome.COLUMN_RGB, rgb);
+            }
+            mContext.getContentResolver().update(MyHomeContract.MyHome.CONTENT_URI, values, MyHomeContract.MyHome.COLUMN_ENTITY_ID + "="+ "'" + entity_id + "'", null);
+        }
+        catch (JSONException e) {
+            Log.e(LOG_TAG, "Failed to parse new light state data");
+        }
     }
 
     @Override
